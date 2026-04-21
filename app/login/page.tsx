55bccb0162a3fd362aef5c704b2d2930edf757ec
Emmanuel Igbeng <emmanueligbeng01@gmail.com>
2026-04-22 00:46:01 +0100

    fixed api typo

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -19,7 +19,7 @@ export default function Login() {
     setLoading(true);
 
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/admin-login/`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/login/`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
